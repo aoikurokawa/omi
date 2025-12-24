ee8399ffb070afc231645060732662a1c5557892
aoikurokawa <ht.so.ik0102@gmail.com>
2025-12-25 04:01:31 +0900

    docs: stake

--- .source/index.js
+++ .source/index.js
@@ -1,8 +1,11 @@
 import { toRuntime } from "fumadocs-mdx"
-import * as file_0 from "../content/docs/meta.json?collection=meta&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
-import * as file_1 from "../content/docs/index.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
-import * as file_2 from "../content/docs/stake-pool.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
-import * as file_3 from "../content/docs/stake-program.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
-import * as file_4 from "../content/docs/stakenet.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
-export const meta = [toRuntime("meta", file_0, {"path":"meta.json","absolutePath":"/Users/aoikurokawa/dev/omi/content/docs/meta.json"})]
-export const docs = [toRuntime("doc", file_1, {"path":"index.mdx","absolutePath":"/Users/aoikurokawa/dev/omi/content/docs/index.mdx"}),toRuntime("doc", file_2, {"path":"stake-pool.mdx","absolutePath":"/Users/aoikurokawa/dev/omi/content/docs/stake-pool.mdx"}),toRuntime("doc", file_3, {"path":"stake-program.mdx","absolutePath":"/Users/aoikurokawa/dev/omi/content/docs/stake-program.mdx"}),toRuntime("doc", file_4, {"path":"stakenet.mdx","absolutePath":"/Users/aoikurokawa/dev/omi/content/docs/stakenet.mdx"})]
+import * as file_0 from "../content/docs/index.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_1 from "../content/docs/stake/index.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_2 from "../content/docs/stake-pool/index.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_3 from "../content/docs/stakenet/index.mdx?collection=docs&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_4 from "../content/docs/meta.json?collection=meta&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_5 from "../content/docs/stake/meta.json?collection=meta&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_6 from "../content/docs/stakenet/meta.json?collection=meta&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+import * as file_7 from "../content/docs/stake-pool/meta.json?collection=meta&hash=feebabf5ebf52a5bdb937a9fa40892357febca0d7e39de35a44ba5a20a2c7874"
+export const docs = [toRuntime("doc", file_0, {"path":"index.mdx","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/index.mdx"}),toRuntime("doc", file_1, {"path":"stake/index.mdx","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/stake/index.mdx"}),toRuntime("doc", file_2, {"path":"stake-pool/index.mdx","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/stake-pool/index.mdx"}),toRuntime("doc", file_3, {"path":"stakenet/index.mdx","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/stakenet/index.mdx"})]
+export const meta = [toRuntime("meta", file_4, {"path":"meta.json","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/meta.json"}),toRuntime("meta", file_5, {"path":"stake/meta.json","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/stake/meta.json"}),toRuntime("meta", file_6, {"path":"stakenet/meta.json","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/stakenet/meta.json"}),toRuntime("meta", file_7, {"path":"stake-pool/meta.json","absolutePath":"/Users/aoi/dev/jito/omi/content/docs/stake-pool/meta.json"})]
